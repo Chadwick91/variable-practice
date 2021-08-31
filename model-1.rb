51db5882231f88722ac063ce7482fc280e16a1d2
Chad Morger <chadmorger@gmail.com>
2021-08-30 20:40:03 -0600

    Add array variables

--- model-1.rb
+++ model-1.rb
@@ -11,3 +11,7 @@ racers = 250
 is_outdoors = true
 is_quiet = false
 is_year_round = false
+
+event_types = ["tuner", "muscle car", "top fuel", "funny car"]
+races = ["1/8th mile", "1/4 mile", "1000 feet"]
+race_type = ["bracket", "heads up"]
